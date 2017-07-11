ab3afeddb495020b538af1d55d1ea5834b343274
Yatheesha S <yatheesha.sunnara.k@gmail.com>
2017-07-11 12:13:31 +0530

    first commit

---
+++ fingersensertest.ino
@@ -0,0 +1,140 @@
+
+#include <Adafruit_Fingerprint.h>
+#include <SoftwareSerial.h>
+
+uint8_t id=1;
+String st="";
+
+// pin #2 is IN from sensor (YELLOW wire)
+// pin #3 is OUT from arduino  (BLUE wire)
+SoftwareSerial mySerial(2, 3);
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+
+void setup()  
+{
+  while (!Serial);  // For Yun/Leo/Micro/Zero/...
+  delay(500);
+  
+  Serial.begin(9600);
+  Serial.println("Adafruit Fingerprint sensor enrollment");
+
+  // set the data rate for the sensor serial port
+  finger.begin(57600);
+  
+  if (finger.verifyPassword()) 
+  {
+    Serial.println("Found fingerprint sensor!");
+  } 
+  else 
+  {
+    Serial.println("Did not find fingerprint sensor :(");
+    while (1);
+  }
+}
+
+
+void loop()                     // run over and over again
+{  
+  while (! getFingerprintEnroll() );
+}
+
+uint8_t getFingerprintEnroll() 
+{
+
+  int p = -1;
+  Serial.println("Waiting for valid finger..................."); 
+  while (p != FINGERPRINT_OK) 
+  {
+    p = finger.getImage();
+    switch (p) 
+    {
+    case FINGERPRINT_OK:
+      Serial.println("Image taken");
+      break;
+    case FINGERPRINT_NOFINGER:
+      break;
+    default:
+      Serial.println("Error while taking image");
+      break;
+    }
+  }
+
+  // OK success!
+
+  p = finger.image2Tz(1);
+  switch (p) 
+  {
+    case FINGERPRINT_OK:
+      Serial.println("Image converted");
+      break;
+    default:
+      Serial.println("Error while converting image");
+      return p;
+  }
+  
+  Serial.println("Remove finger");
+  delay(1000);
+  p = 0;
+  while (p != FINGERPRINT_NOFINGER) 
+  {
+    p = finger.getImage();
+  }
+  
+  p = -1;
+  Serial.println("Place same finger again..................");
+  while (p != FINGERPRINT_OK) 
+  {
+    p = finger.getImage();
+    switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image taken");
+      break;
+    case FINGERPRINT_NOFINGER:
+      break;
+    default:
+      Serial.println("Error while taking image");
+      break;
+    }
+  }
+
+  // OK success!
+
+  p = finger.image2Tz(2);
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image converted");
+      break;
+    default:
+      Serial.println("Error while converting image");
+      return p;
+  }
+  
+  // OK converted!
+  Serial.print("Creating model for #");  
+  Serial.println(id);
+  
+  p = finger.createModel();
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Prints matched!");
+  } else if (p == FINGERPRINT_ENROLLMISMATCH) {
+    Serial.println("Fingerprints did not match");
+    return p;
+  } else {
+    Serial.println("Error while matching image");
+    return p;
+  }   
+  
+  Serial.print("ID "); 
+  Serial.println(id);
+  p = finger.storeModel(id);
+  if (p == FINGERPRINT_OK) 
+  {
+    Serial.println("Success!");
+    uploadFingerpintTemplate(id);    
+  } 
+  else 
+  {
+    Serial.println("Error while storing image");
+    return p;
+  }   
+}
---
+++ loadid.ino
@@ -0,0 +1,35 @@
+uint8_t uploadFingerpintTemplate(uint16_t id)
+{
+ uint8_t p = finger.loadModel(id);
+ if(p!=FINGERPRINT_OK)
+ {
+    Serial.print("Error while loading "); 
+    return p;
+ }     
+  // OK success!
+
+  p = finger.getModel();
+  if(p!=FINGERPRINT_OK)
+ {
+    Serial.print("Error while getting Model"); 
+    return p;
+ }   
+  
+  uint8_t templateBuffer[256];
+  memset(templateBuffer, 0xff, 256);  //zero out template buffer
+  int index=0;
+  uint32_t starttime = millis();
+  while (index < 256)
+  {
+    if (mySerial.available())
+    {
+      templateBuffer[index] = mySerial.read();
+      st+=templateBuffer[index];
+      index++;
+    }
+  }
+  Serial.print("\nYour ID:");
+  Serial.println(st);
+  Serial.println();
+  st="";
+}
